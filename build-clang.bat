bfbd74670690de495095e28ce66d8def7fcf5cab
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-09-03 07:47:24 +0100

    Rename _empty.c

--- build-clang.bat
+++ build-clang.bat
@@ -3,13 +3,13 @@ mkdir lib
 cd src
 clang -c -I..\include -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS -D_CRT_DECLARE_NONSTDC_NAMES=0 -D_WPDK_BUILD_LIB_ -g *.c
 
-lib /out:empty.lib _empty.o
+lib /out:empty.lib empty.o
 copy empty.lib ..\lib\numa.lib
 copy empty.lib ..\lib\rt.lib
 copy empty.lib ..\lib\uuid.lib
 copy empty.lib ..\lib\pthread.lib
 move empty.lib ..\lib\crypto.lib
-del _empty.o
+del empty.o
 
 llvm-ar crDs wpdk.lib *.o
 move wpdk.lib ..\lib\wpdk.lib
--- build.bat
+++ build.bat
@@ -3,13 +3,13 @@ mkdir lib
 cd src
 cl -c -I..\include -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS -D_CRT_DECLARE_NONSTDC_NAMES=0 -D_WPDK_BUILD_LIB_ *.c
 
-lib /out:empty.lib _empty.obj
+lib /out:empty.lib empty.obj
 copy empty.lib ..\lib\numa.lib
 copy empty.lib ..\lib\rt.lib
 copy empty.lib ..\lib\uuid.lib
 copy empty.lib ..\lib\pthread.lib
 move empty.lib ..\lib\crypto.lib
-del _empty.obj
+del empty.obj
 
 lib /out:wpdk.lib *.obj
 move wpdk.lib ..\lib\wpdk.lib
