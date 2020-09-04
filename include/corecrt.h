27516c293276835fd3f2332ecc367fda3b836fa2
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-09-04 08:14:58 +0100

    Remove need for command line definitions for clean compile

---
+++ include/corecrt.h
@@ -0,0 +1,9 @@
+#ifndef _WPDK_CORECRT_H_
+#define _WPDK_CORECRT_H_
+
+#define _CRT_DECLARE_NONSTDC_NAMES	0
+#define _CRT_SECURE_NO_WARNINGS		1
+
+#include <../ucrt/corecrt.h>
+
+#endif /* !_WPDK_CORECRT_H_ */
