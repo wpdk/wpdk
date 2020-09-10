fcc53c6f894a1d40f81c605eeec22119af963e02
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-09-10 23:11:55 +0100

    Initial meson build script

--- build.bat
+++ build.bat
@@ -1,19 +1,66 @@
 @echo off
+@setlocal enableextensions
 
-mkdir lib
+if "%VCINSTALLDIR%"=="" (
+	echo "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+	goto :eof
+)
+
+rmdir /s /q build
+rmdir /s /q build-tmp
+
+set CC=cl
+set DESTDIR=%CD%\build
+
+meson --buildtype=debug build-tmp
+ninja -C build-tmp install
+
+goto :eof
+
+rem **********************************
+rem old script for cl
+rem **********************************
+
+mkdir build\lib
 
 cd src
 cl /nologo -c -Z7 -I..\include *.c
 
 lib /nologo /out:empty.lib empty.obj
-copy empty.lib ..\lib\numa.lib >nul:
-copy empty.lib ..\lib\rt.lib >nul:
-copy empty.lib ..\lib\uuid.lib >nul:
-copy empty.lib ..\lib\pthread.lib >nul:
-move empty.lib ..\lib\crypto.lib >nul:
+copy empty.lib ..\build\lib\numa.lib >nul:
+copy empty.lib ..\build\lib\rt.lib >nul:
+copy empty.lib ..\build\lib\uuid.lib >nul:
+copy empty.lib ..\build\lib\pthread.lib >nul:
+move empty.lib ..\build\lib\crypto.lib >nul:
 del empty.obj
 
 lib /nologo /out:wpdk.lib *.obj
-move wpdk.lib ..\lib\wpdk.lib >nul:
+move wpdk.lib ..\build\lib\wpdk.lib >nul:
 del *.obj
 cd ..
+
+robocopy include build\include /MIR /NDL /NP /R:5 /W:5 /NJH /NJS /NFL
+
+rem **********************************
+rem old script for clang
+rem **********************************
+
+mkdir build\lib
+
+cd src
+clang -c -I..\include -g *.c
+
+lib /nologo /out:empty.lib empty.o
+copy empty.lib ..\build\lib\numa.lib >nul:
+copy empty.lib ..\build\lib\rt.lib >nul:
+copy empty.lib ..\build\lib\uuid.lib >nul:
+copy empty.lib ..\build\lib\pthread.lib >nul:
+move empty.lib ..\build\lib\crypto.lib >nul:
+del empty.o
+
+llvm-ar crDs wpdk.lib *.o
+move wpdk.lib ..\build\lib\wpdk.lib >nul:
+del *.o
+cd ..
+
+robocopy include build\include /MIR /NDL /NP /R:5 /W:5 /NJH /NJS /NFL
---
+++ scripts/copy_libs.bat
@@ -0,0 +1,8 @@
+@echo off
+
+copy %DESTDIR%\lib\libcrypto.a %DESTDIR%\lib\crypto.lib >nul:
+copy %DESTDIR%\lib\libnuma.a %DESTDIR%\lib\numa.lib >nul:
+copy %DESTDIR%\lib\libpthread.a %DESTDIR%\lib\pthread.lib >nul:
+copy %DESTDIR%\lib\librt.a %DESTDIR%\lib\rt.lib >nul:
+copy %DESTDIR%\lib\libuuid.a %DESTDIR%\lib\uuid.lib >nul:
+copy %DESTDIR%\lib\libwpdk.a %DESTDIR%\lib\wpdk.lib >nul:
