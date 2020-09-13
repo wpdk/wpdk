b74ef943a8022121d7a8655bc3afe03b2d708fe5
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-09-13 11:53:38 +0100

    Add CUnit subproject and build it

--- scripts/copy_libs.bat
+++ scripts/copy_libs.bat
@@ -1,5 +1,7 @@
 @echo off
 
+copy %DESTDIR%\lib\libcunit.a %DESTDIR%\lib\cunit.lib >nul:
+
 copy %DESTDIR%\lib\libcrypto.a %DESTDIR%\lib\crypto.lib >nul:
 copy %DESTDIR%\lib\libnuma.a %DESTDIR%\lib\numa.lib >nul:
 copy %DESTDIR%\lib\libpthread.a %DESTDIR%\lib\pthread.lib >nul:
