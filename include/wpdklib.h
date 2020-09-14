921590ff8bbdc32f8968a8292def0f7d75a691bb
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-09-14 18:30:23 +0100

    Minor header fixes

--- include/wpdklib.h
+++ include/wpdklib.h
@@ -6,6 +6,8 @@
 #include <sys/platform.h>
 #include <sys/socket.h>
 
+_WPDK_BEGIN_C_HEADER
+
 int wpdk_socket_startup();
 
 int wpdk_is_socket(int fd);
@@ -27,5 +29,7 @@ ssize_t wpdk_socket_writev(int fildes, const struct iovec *iov, int iovcnt);
 const char *wpdk_get_path(const char *path, char *buffer, size_t len);
 
 int wpdk_convert_to_errno(int err);
+
+_WPDK_END_C_HEADER
 
 #endif /* _WPDK_WPDKLIB_H_ */
