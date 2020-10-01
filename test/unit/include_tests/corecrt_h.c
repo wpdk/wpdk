3790fad9aa997db92dccfda8a60bee2c1c71e1dd
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-10-01 15:40:17 +0100

    Auto generate cpp include tests Reduces overhead to add tests for a new header file

---
+++ test/unit/include_tests/corecrt_h.c
@@ -0,0 +1,31 @@
+/*-
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Copyright (c) 2020, MayaData Inc. All rights reserved.
+ *  Copyright (c) 2020, DataCore Software Corporation. All rights reserved.
+ * 
+ *  POSIX details are based on the Open Group Base Specification Issue 7,
+ *  2018 edition at https://pubs.opengroup.org/onlinepubs/9699919799/
+ * 
+ *  Details about Linux extensions are based on the Linux man-pages project
+ *  at https://www.kernel.org/doc/man-pages/
+ */
+
+#ifndef __MINGW32__
+#include <corecrt.h>
+#else
+#include <sys/types.h>
+#endif
+
+#include <CUnit/Basic.h>
+
+#ifdef __cplusplus
+#define include_c_corecrt_h include_cpp_corecrt_h
+#endif
+
+
+void
+include_c_corecrt_h(void)
+{
+	CU_ASSERT(sizeof(errno_t) != 0);
+}
---
+++ test/unit/include_tests/setjmp_h.c
@@ -0,0 +1,34 @@
+/*-
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Copyright (c) 2020, MayaData Inc. All rights reserved.
+ *  Copyright (c) 2020, DataCore Software Corporation. All rights reserved.
+ * 
+ *  POSIX details are based on the Open Group Base Specification Issue 7,
+ *  2018 edition at https://pubs.opengroup.org/onlinepubs/9699919799/
+ * 
+ *  Details about Linux extensions are based on the Linux man-pages project
+ *  at https://www.kernel.org/doc/man-pages/
+ */
+
+#include <setjmp.h>
+
+#include <CUnit/Basic.h>
+
+#ifdef __cplusplus
+#define include_c_setjmp_h include_cpp_setjmp_h
+#endif
+
+#if defined(_MSC_VER) && defined(__cplusplus)
+#pragma warning(disable : 4611)
+#endif
+
+
+void
+include_c_setjmp_h(void)
+{
+	sigjmp_buf env;
+	sigsetjmp(env, 0);
+
+	CU_ASSERT(sizeof(sigjmp_buf) == sizeof(jmp_buf));
+}
