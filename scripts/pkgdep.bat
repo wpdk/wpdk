584b00c3b079b778afa25c026bdea097a198f805
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-11-23 13:51:46 +0000

    Add pkgdep script

---
+++ scripts/pkgdep.bat
@@ -0,0 +1,87 @@
+@echo off
+@setlocal enableextensions enabledelayedexpansion
+
+net session >nul: 2>&1
+if errorlevel 1 (
+	echo %0 must be run with Administrator privileges
+	goto :eof
+)
+
+set "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+where /q choco
+if errorlevel 1 (
+	powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+	if errorlevel 1 goto :eof
+)
+
+set "PATH=%PATH%;%ProgramFiles%\Git\bin"
+where /q git
+if errorlevel 1 (
+	echo Installing Git ...
+	choco install git -y -r
+	if errorlevel 1 goto :eof
+)
+
+set "PATH=%PATH%;%ProgramFiles%\MESON"
+where /q meson
+if errorlevel 1 (
+	echo Installing Meson ...
+	choco install meson -y -r
+	if errorlevel 1 goto :eof
+)
+
+where /q gcc
+if errorlevel 1 (
+	echo Installing MinGW ...
+	choco install mingw -y -r
+	if errorlevel 1 goto :eof
+)
+
+set "PATH=%PATH%;%ProgramFiles%\NASM"
+where /q nasm
+if errorlevel 1 (
+	echo Installing NASM ...
+	choco install nasm -y -r
+	if errorlevel 1 goto :eof
+)
+
+set "PATH=%PATH%;%SystemDrive%\tools\msys64"
+where /q msys2
+if errorlevel 1 (
+	echo Installing MSYS2 ...
+	choco install msys2 -y -r
+	if errorlevel 1 goto :eof
+)
+
+set MSYS2=call msys2_shell -no-start -here -use-full-path -defterm
+
+set ver=
+( for /f "tokens=*" %%f in ('%MSYS2% -c "make --version 2>/dev/null"') do (set ver=%%f) ) 2>nul:
+if "%ver%"=="" (
+	echo Installing make ...
+	%MSYS2% -c "pacman --noconfirm -S msys/make"
+)
+
+set ver=
+( for /f "tokens=*" %%f in ('%MSYS2% -c "python3 --version 2>/dev/null"') do (set ver=%%f) ) 2>nul:
+if "%ver%"=="" (
+	echo Installing python ...
+	%MSYS2% -c "pacman --noconfirm -S msys/python"
+)
+
+set ver=
+( for /f "tokens=*" %%f in ('%MSYS2% -c "cmp --version 2>/dev/null"') do (set ver=%%f) ) 2>nul:
+if "%ver%"=="" (
+	echo Installing diffutils ...
+	%MSYS2% -c "pacman --noconfirm -S msys/diffutils"
+)
+
+goto :eof
+
+set ver=
+( for /f "tokens=*" %%f in ('clang --version') do (set ver=%%f) ) 2>nul:
+if "%ver%"=="" (
+	echo Installing Clang ...
+	choco install llvm -y -r
+	if errorlevel 1 goto :eof
+)
