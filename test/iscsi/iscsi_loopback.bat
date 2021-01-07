f1a0944dad21bda9c6e1af27631670f023861451
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-01-07 22:50:31 +0000

    Add path to shared libraries

--- test/iscsi/iscsi_loopback.bat
+++ test/iscsi/iscsi_loopback.bat
@@ -1,4 +1,5 @@
 @echo off
 @setlocal enableextensions enabledelayedexpansion
 
+set "PATH=%~dp0\..\..\..\dpdk\build\bin;%PATH%"
 "%~dp0\..\..\..\build\bin\iscsi_tgt.exe" -c "%~dp0\iscsi_loopback.json"
