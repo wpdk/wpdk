8a3074f69a69ce16d780048be4a58968585029a2
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-01-08 11:16:32 +0000

    Remove .d files as part of clean

--- build.bat
+++ build.bat
@@ -69,6 +69,7 @@ if not "%CLEAN%"=="" (
 		if exist %%i\build-tmp rmdir /s /q %%i\build-tmp >nul:
 	)
 	if exist mk\config.mk del /q mk\config.mk >nul:
+	if "%SPDK%"=="y" del /q /s *.d >nul: 2>&1 
 )
 
 if "%CLEAN%"=="clean" goto :eof
