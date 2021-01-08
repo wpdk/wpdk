b4294dfefe5e662a7abbb0a0ed24c1cb5da51d04
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-01-08 15:33:34 +0000

    Add nvmf test script

---
+++ test/nvmf/nvmf_tgt.bat
@@ -0,0 +1,11 @@
+@echo off
+@setlocal enableextensions enabledelayedexpansion
+
+findstr /L LOCAL-IP "%~dp0\nvmf_tgt.json" > nul:
+if not errorlevel 1 (
+	echo Replace LOCAL-IP in nvmf_tgt.json with local IP address
+	goto :eof
+)
+
+set "PATH=%~dp0\..\..\..\dpdk\build\bin;%PATH%"
+"%~dp0\..\..\..\build\bin\nvmf_tgt.exe" -c "%~dp0\nvmf_tgt.json"
