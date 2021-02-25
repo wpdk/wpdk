f2e41bcc4267a4423bf39967435ad218d27a0818
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-02-25 10:58:54 +0000

    Add support for GCC stack guard

---
+++ src/stack.c
@@ -0,0 +1,23 @@
+/*-
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Copyright (c) 2020, MayaData Inc. All rights reserved.
+ *  Copyright (c) 2020, DataCore Software Corporation. All rights reserved.
+ * 
+ *  POSIX details are based on the Open Group Base Specification Issue 7,
+ *  2018 edition at https://pubs.opengroup.org/onlinepubs/9699919799/
+ * 
+ *  Details about Linux extensions are based on the Linux man-pages project
+ *  at https://www.kernel.org/doc/man-pages/
+ */
+
+#include <wpdk/internal.h>
+#include <stdint.h>
+
+uint64_t __stack_chk_guard = 0x0BAD1BAD2BAD3BADLL;
+
+void
+__stack_chk_fail(void)
+{
+	WPDK_FATAL_ERROR("Stack corruption detected");
+}
