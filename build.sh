2827d38541a6dfcadba6bc404306e3f709bf06d6
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-03-04 12:16:52 +0000

    Add support for independent builds

--- build.sh
+++ build.sh
@@ -18,7 +18,14 @@ DESTDIR=
 [ "$SPDK" != "y" ] && DESTDIR=`pwd`/build
 
 CONFDIR=build
-[ "$SPDK" != "y" -a -d ../dpdkbuild ] && CONFDIR=../build
+if [ "$SPDK" != "y" ]
+then
+	if [ -d ../dpdkbuild ]
+	then CONFDIR=../build
+	elif [ -d ../spdk/dpdkbuild ]
+	then CONFDIR=../spdk/build
+	fi
+fi
 
 cfg=
 [ -f $CONFDIR/_config ] && cfg=`cat $CONFDIR/_config | tr -d '\r'`
@@ -43,7 +50,7 @@ done
 
 if [ "$CLEAN" != "clean" ]
 then
-	[ ! -f mk/config.mk ] && CLEAN=y
+	[ "$SPDK" = "y" -a ! -f mk/config.mk ] && CLEAN=y
 	if [ "$cfg" != "$CC $ARCH $TYPE" ]
 	then
 		echo Config changed from "$cfg" to "$CC $ARCH $TYPE"
@@ -54,13 +61,17 @@ fi
 if [ -n "$CLEAN" ]
 then
 	echo Cleaning...
-	for i in . dpdp wpdk
-	do
-		rm -rf $i/build $i/build-tmp
+	for i in . dpdp wpdk "$WPDKDIR"
+	do rm -rf $i/build $i/build-tmp
 	done
 	rm -f mk/config.mk
 	find . -type f -name \*.d -print | xargs rm -f
 	"$WPDKDIR/scripts/symlink_exe.sh" rm
+	(
+		cd "$WPDKDIR"
+		find . -type f -name \*.d -print | xargs rm -f
+		./scripts/symlink_exe.sh rm
+	)
 fi
 
 [ "$CLEAN" = "clean" ] && exit 0
@@ -85,6 +96,13 @@ if [ "$SPDK" = "y" ]
 then
 	CONFIG_OPTS="--cross-prefix=x86_64-w64-mingw32"
 	[ "$TYPE" == "debug" ] && CONFIG_OPTS="$CONFIG_OPTS --enable-debug"
+	
+	if [ ! -d wpdk -a -d "$WPDKDIR" ]
+	then
+		(cd "$WPDKDIR"; ./build.sh )
+		CONFIG_OPTS="$CONFIG_OPTS --without-isal --with-wpdk=$WPDKDIR/build"
+	fi
+
 	[ ! -f mk/config.mk ] && CC=gcc ./configure $CONFIG_OPTS
 	make -j8
 	"$WPDKDIR/scripts/symlink_exe.sh"
