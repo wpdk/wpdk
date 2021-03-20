50cd0d39d761b60d98d514c043b1608e5ecf0ccc
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-03-20 11:11:36 +0000

    Add bashrc to build

---
+++ scripts/bashrc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021, MayaData Inc. All rights reserved.
+# Copyright (c) 2021, DataCore Software Corporation. All rights reserved.
+
+wpdk_bin=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+if [[ -e $wpdk_bin/wpdk_kill.sh ]]; then
+	# shellcheck disable=SC2139
+	alias kill="$wpdk_bin/wpdk_kill.sh"
+fi
+
+alias uname='echo Windows ;:'
