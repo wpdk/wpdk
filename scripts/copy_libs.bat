a52f91e569b806341a355eaf650cb38b144d8622
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2022-05-25 12:39:54 +0100

    Create empty ssl library
    Signed-off-by: Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>

--- scripts/copy_libs.bat
+++ scripts/copy_libs.bat
@@ -7,4 +7,5 @@ copy lib\libcrypto.a lib\crypto.lib > nul:
 copy lib\libnuma.a lib\numa.lib > nul:
 copy lib\libpthread.a lib\pthread.lib > nul:
 copy lib\librt.a lib\rt.lib > nul:
+copy lib\libssl.a lib\ssl.lib > nul:
 copy lib\libuuid.a lib\uuid.lib > nul:
